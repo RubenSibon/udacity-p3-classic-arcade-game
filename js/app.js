c3f51866cbeed53321e4b53d772ffb50586bd340
rubenJS <mail@rubensibon.nl>
2016-12-02 18:11:31 +0100

    Add Roadster super class & TO DO list

--- js/app.js
+++ js/app.js
@@ -1,27 +1,50 @@
-// Enemies our player must avoid
+/* TO DO:
+    - Add gems;
+    - Add scoring;
+    - Add math functions for all moving objects;
+    - Add levels that increase difficulty by increasing the max-speed of enemies, allowing for more enemies at a time and randomly generating them more often;
+*/
+
+// Game class so to allow for scoring and levels. At a later point the game's state could be saved (TO DO).
+var Game = function() {
+    this.level = 1; // Level number is also the difficulty parameter.
+    this.score = 0;
+}
+
+Game.prototype.nextLevel = function() {
+    this.score += this.level;
+    this.level++;
+}
+
+// Super class for all NPC's that move over the road.
+var Roadster = function() {};
+
+// Roadsters start randomly at either one of three lanes on the y-axis.
+Roadster.prototype.startLane = function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    var randNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (randNum == 1) {
+      return 62;
+    } else if (randNum == 2) {
+      return 145;
+    } else if (randNum == 3) {
+      return 227;
+    }
+};
+
+// Enemies our player must avoid. A sub class of Roadster.
 var Enemy = function() {
     this.x = -101;
-
-    // Roadsters start randomly at either y = 80, y = 160, y = 240.
-    var startLane = function(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      var randNum = Math.floor(Math.random() * (max - min + 1)) + min;
-      if (randNum == 1) {
-          return 62;
-      } else if (randNum == 2) {
-          return 145;
-      } else if (randNum == 3) {
-          return 227;
-      }
-    }
-    this.y = startLane(1,3);
+    this.y = this.startLane(1, 3);
     this.speed = Math.random() * 500;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+Enemy.prototype = Object.create(Roadster.prototype);
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
