17e3208963d19fbc08a7da3baa534692f412136e
rubenJS <mail@rubensibon.nl>
2016-12-02 18:31:06 +0100

    Improve y-positioning math and some comment cleanup

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@
     - Add gems;
     - Add scoring;
     - Add math functions for all moving objects;
-    - Add levels that increase difficulty by increasing the max-speed of enemies, allowing for more enemies at a time and randomly generating them more often;
+    - Add levels that increase difficulty by:
+        - letting enenmies come from the left and the right,
+        - allowing for more enemies at a time,
+        - randomly generating them more often;
 */
 
-// Game class so to allow for scoring and levels. At a later point the game's state could be saved (TO DO).
+// Game object so to allow for scoring and levels. At a later point the game's state could be saved (TO DO).
 var Game = function() {
     this.level = 1; // Level number is also the difficulty parameter.
     this.score = 0;
@@ -16,6 +19,8 @@ Game.prototype.nextLevel = function() {
     this.level++;
 }
 
+
+
 // Super class for all NPC's that move over the road.
 var Roadster = function() {};
 
@@ -24,23 +29,16 @@ Roadster.prototype.startLane = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     var randNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    if (randNum == 1) {
-      return 62;
-    } else if (randNum == 2) {
-      return 145;
-    } else if (randNum == 3) {
-      return 227;
-    }
+    return 83 * randNum - 20; // 83 is the height of a row.
 };
+
+
 
 // Enemies our player must avoid. A sub class of Roadster.
 var Enemy = function() {
     this.x = -101;
     this.y = this.startLane(1, 3);
     this.speed = Math.random() * 500;
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -49,9 +47,6 @@ Enemy.prototype = Object.create(Roadster.prototype);
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x = this.x + (this.speed * dt);
 }
 
@@ -59,6 +54,8 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -74,6 +71,8 @@ Player.prototype.render = function() {
 };
 Player.prototype.handleInput = function() {};
 
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -82,6 +81,7 @@ var enemy = allEnemies.push(new Enemy());
 
 var player = new Player(300, 400);
 // player.render();
+
 
 
 // This listens for key presses and sends the keys to your
